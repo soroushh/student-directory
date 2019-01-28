52503f4026e47fd0bee1684c9dc1dcaf70ad327a
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-28 16:00:07 +0000

    saved some data to the file

--- directory.rb
+++ directory.rb
@@ -33,6 +33,7 @@ end
 def print_menu
   puts "1. Input students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we will be adding more items
 end
 
@@ -48,11 +49,23 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
     puts "I don't know what you mean, try again"
   end
+end
+
+def save_students
+  file = File.open("students.csv","w")
+  @students.each do |student|
+    student_data = [student[:name],student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close()
 end
 
 def interactive_menu
