72b49e8734720b0ebbb6ea59dd95d9e0f8d7c4ee
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-31 10:09:14 +0000

    Exercise 3 of step14 was solved.

--- step14Execises.rb
+++ step14Execises.rb
@@ -1,19 +1,24 @@
 @students = [] #an empty array accessible to all methods.
+
 def add_a_student(name, cohort = "november")
   @students << {name: name, cohort: cohort.to_sym}
 end
+
+def input_taking
+  STDIN.gets.chomp()
+end
+
 def input_students
-  puts "Please enter the names of students."
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of students.\nTo finish, just hit return twice"
   # get the first name
-  name = STDIN.gets.chomp
+  name = input_taking
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
     add_a_student(name)
     puts "Now we have #{@students.count} students"
     #get another name from the user
-    name = STDIN.gets.chomp
+    name = input_taking
   end
 end
 
@@ -33,11 +38,8 @@ def print_footer
 end
 
 def print_menu
-  puts "1. Input students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts"4. Load the list from students.csv"
-  puts "9. Exit" # 9 because we will be adding more items
+  puts "1. Input students\n2. Show the students \n3. Save the list to students.csv\n4. Load the list from students.csv\n9. Exit"
+   # 9 because we will be adding more items
 end
 
 def show_students()
